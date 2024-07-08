38e3202e363056fb134fb8edbfc48ed035767211
Karmele <ktelletxea.kt@gmail.com>
2024-07-08 11:36:16 +0200

    antes de descanso

---
+++ src/libs/component.js
@@ -0,0 +1,59 @@
+export class miComponenteWeb extends HTMLElement {
+    constructor() {
+        super();
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <div class="article-card">
+                <img class="article-image" src="" alt="">
+            </div>
+            <style>
+          
+                .article-card {
+                    width: calc( 25rem);
+                    box-sizing: border-box;
+                    text-align: center;
+                    background-color: #f9f9f9;
+                    transition: background-color 0.3s ease;
+                    border-radius: 1rem;
+                    padding: 1rem;
+                    margin: 2rem;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+
+                .article-image {
+                    width: 100%;
+                    height: auto;
+                    max-height: 12rem;
+                    object-fit: cover;
+                    border-radius: 1rem;
+                }
+
+                .article-card:hover {
+                    background-color: #c5c4c4;
+                }
+                    @media only screen and (max-width:800px) {
+                 .article-card {
+                    width: calc( 15rem);
+                    padding: 0.5rem;
+                    margin: 1rem;
+                    margin-rigth:10rem
+                }
+                    }
+            </style>
+        `;
+
+        const root = this.attachShadow({ mode: "open" });
+        root.appendChild(document.importNode(template.content, true))
+    }
+
+    connectedCallback() {
+        const imageSrc = this.getAttribute('image-src');
+        const shadow = this.shadowRoot;
+        shadow.querySelector('.article-image').src = imageSrc;
+        shadow.querySelector('.article-image').alt = title;
+    }
+}
+
---
+++ src/libs/store.js
@@ -0,0 +1,61 @@
+// Asegúrate de que la declaración de importación esté al nivel superior
+import { miComponenteWeb } from './component.js';
+
+// Define el elemento personalizado
+window.customElements.define('custom-tag', miComponenteWeb);
+
+// Comprobación de autenticación
+const urlauth = new URLSearchParams(window.location.search);
+const authen = urlauth.get('auth');
+if (!authen) {
+    alert('Registrate');
+    window.location = 'login.html';
+}
+
+// Evento para cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function () {
+    const contenedorlib = document.getElementById('containerlib');
+    const lista = document.getElementById('lista');
+
+    // Al iniciar, carga los artículos de 'Biografia'
+    cargarArticulos();
+
+    function cargarArticulos() {
+        fetch('/static/libros.json')
+            .then(response => response.json())
+            .then(data => {
+                renderizarArticulos(data.Biografia);
+                agregarEventosFiltros(data);
+            })    
+            .catch(error => console.error('Error al cargar los artículos:', error)); // Añade manejo de errores
+    }
+
+    // Renderiza los artículos utilizando los componentes web importados
+    function renderizarArticulos(articulos) {
+        contenedorlib.innerHTML = ''; // Refresca el contenedor
+        // Por cada artículo, crea un 'custom-tag'
+        articulos.forEach(articulo => {
+            const articleElement = document.createElement('custom-tag');
+            articleElement.setAttribute('image-src', articulo.image);
+            /* Añadir evento de clic para llevar a otra página HTML
+            articleElement.addEventListener('click', function () {
+                window.location.href =  `articulos.html?labels=${articulo.labels}`;
+                // Seguramente tendrás que llevar el tag o id para identificar
+            });
+            */
+            contenedorlib.appendChild(articleElement);
+        });
+    }
+
+    // Filtra los artículos por categoría
+    function agregarEventosFiltros(data) {
+        lista.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (event.target.tagName === 'A') {
+                const categoria = event.target.dataset.category;
+                const articulosFiltrados = data[categoria];
+                renderizarArticulos(articulosFiltrados);
+            }
+        });
+    }
+});
